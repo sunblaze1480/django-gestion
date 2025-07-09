9d87fd45b42b4eb93b9fbc8c73eb5265ebf62a7a
Emanuel Rodríguez Martí <emanuel.rodriguezmq@gmail.com>
2025-07-09 15:54:14 -0300

    fixing allowed hosts

--- web_ventas/web_ventas/settings.py
+++ web_ventas/web_ventas/settings.py
@@ -19,8 +19,7 @@ SECRET_KEY = "django-insecure-gh62$46+um)16s03go+$z+em+-(2zxv0ao$z(pk+!#8bj+yerz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['your-server-ip', 'your-domain.com', 'localhost', '127.0.0.1', '10.8.0.1']
 
 # Application definition
 
